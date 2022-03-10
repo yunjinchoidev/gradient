4e8a912462872195a5aa7ab5de0544662b267a50
HoonJoo Jang <hoonjoo1@naver.com>
2022-03-10 21:29:26 +0900

    2022-03-10 장훈주 Risk dao, mapper, service 생성

---
+++ src/main/java/project5/risk/RiskDao.java
@@ -0,0 +1,8 @@
+package project5.risk;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RiskDao {
+
+}
---
+++ src/main/java/project5/risk/RiskService.java
@@ -0,0 +1,8 @@
+package project5.risk;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RiskService {
+
+}
